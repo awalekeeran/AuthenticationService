7e08871fc805858b246fc36e7c0b51ff893c792d
Kiran Awale <Kiran.Awale@wolterskluwer.com>
2023-04-16 22:49:21 +0530

    Added City Controller

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private static readonly string[] Cities = new[]
+        {
+            "Atlanta", "New York", "Pune", "Bengaluru"
+        };
+
+        private readonly ILogger<CityController> _logger;
+
+        public CityController(ILogger<CityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCity")]
+        public IEnumerable<string> Get()
+        {
+            return Cities;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Cities[id];
+        }
+    }
+}
