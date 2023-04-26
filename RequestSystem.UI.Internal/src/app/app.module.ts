723358ff796ed15bcde266723ba8df2c82b35b14
Kiran Awale <Kiran.Awale@wolterskluwer.com>
2023-04-26 23:43:15 +0530

    Implemented logging interceptor

--- RequestSystem.UI.Internal/src/app/app.module.ts
+++ RequestSystem.UI.Internal/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './shared/accordion.component';
+import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { AccordionComponent } from './shared/accordion.component';
   imports: [
     BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
